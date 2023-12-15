3fc96734473d465aa6c1791706d7a19d831bbda3
Arun Sankar <sankarchevoor@gmail.com>
2023-12-15 21:56:58 +0530

    initial commit

---
+++ models/genre.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+    name:{type:String, required:true},  
+})
+
+GenreSchema.virtual("url").get(function () {
+    return `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model("Genre", GenreSchema);
