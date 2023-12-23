15f0aa1bf790be8e49860606edf18f7adc79a403
Arun Sankar <sankarchevoor@gmail.com>
2023-12-23 20:44:41 +0530

    finished

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -158,12 +158,45 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Book delete GET");
+    // Get details of book and all their instances (in parallel)
+    const [book, allBookInstances] = await Promise.all([
+        Book.findById(req.params.id).exec(),
+        BookInstance.find({ book: req.params.id }, "status book").exec(),
+    ]);
+
+    if (book === null) {
+        // No results.
+        res.redirect("/catalog/books");
+    }
+
+    res.render("book_delete", {
+        title: "Delete Book",
+        book: book,
+        book_instances: allBookInstances,
+    });
 });
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Book delete POST");
+    // Get details of book and all their instances (in parallel)
+    const [book, allBookInstances] = await Promise.all([
+        Book.findById(req.params.id).exec(),
+        BookInstance.find({ book: req.params.id }, "status book").exec(),
+    ]);
+
+    if (allBookInstances.length > 0) {
+        //  book has copies. Render in same way as for GET route.
+        res.render("book_delete", {
+            title: "Delete Book",
+            book: book,
+            book_instances: allBookInstances,
+        });
+        return;
+    } else {
+        // Author has no books. Delete object and redirect to the list of authors.
+        await Book.findByIdAndDelete(req.body.bookid);
+        res.redirect("/catalog/books");
+    }
 });
 
 // Display book update form on GET.
